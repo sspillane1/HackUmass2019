be09a50af7a814980c3ca837947bd07780e90651
Ryan Wickett <ryan@wickettonline.com>
2019-10-20 10:02:28 -0400

    fixed the damn button

--- hello/www/js/video.js
+++ hello/www/js/video.js
@@ -5,3 +5,10 @@
  function closeNav() {
      document.getElementById("mySideNav").style.width = "0";
  }
+ window.onload = _init;
+function _init() {
+  //debugger;
+  document.getElementById("coolbtn").addEventListener("click", function() {
+    openNav()
+  })
+}
