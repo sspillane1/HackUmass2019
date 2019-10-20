104390e1028486ed0b73630d6666befc868ddb27
Ryan Wickett <ryan@wickettonline.com>
2019-10-20 03:42:58 -0400

    Attempt

--- hello/www/js/video.js
+++ hello/www/js/video.js
@@ -1,27 +1,98 @@
-let app = {
-  initialize: function(){
-    app.onDeviceReady()
+var app = {
+  startCameraAbove: function(){
+    CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 300, toBack: false, previewDrag: true, tapPhoto: true});
   },
 
-  onDeviceReady: function(){
-    let options = {
-      x: 0,
-      y: 0,
-      width: window.screen.width,
-      height: window.screen.height,
-      toBack: false,
-      tapPhoto: true,
-      tapFocus: false,
-      previewDrag: false,
-      storeToFile: false,
-      disableExifHeaderStripping: false
-    };
+  startCameraBelow: function(){
+    CameraPreview.startCamera({x: 50, y: 50, width: 300, height:300, camera: "front", tapPhoto: true, previewDrag: false, toBack: true});
+  },
 
-    CameraPreview.startCamera(options);
-    CameraPreview.show(document.write("Success"),document.write("Failed"));
+  stopCamera: function(){
+    CameraPreview.stopCamera();
+  },
+
+  takePicture: function(){
+    CameraPreview.takePicture(function(imgData){
+      document.getElementById('originalPicture').src = 'data:image/jpeg;base64,' + imgData;
+    });
+  },
+
+  switchCamera: function(){
+    CameraPreview.switchCamera();
+  },
+
+  show: function(){
+    CameraPreview.show();
+  },
+
+  hide: function(){
+    CameraPreview.hide();
+  },
+
+  changeColorEffect: function(){
+    var effect = document.getElementById('selectColorEffect').value;
+    CameraPreview.setColorEffect(effect);
+  },
+
+  changeFlashMode: function(){
+    var mode = document.getElementById('selectFlashMode').value;
+    CameraPreview.setFlashMode(mode);
+  },
+
+  changeZoom: function(){
+    var zoom = document.getElementById('zoomSlider').value;
+    document.getElementById('zoomValue').innerHTML = zoom;
+    CameraPreview.setZoom(zoom);
+  },
+
+  changePreviewSize: function(){
+    window.smallPreview = !window.smallPreview;
+    if(window.smallPreview){
+      CameraPreview.setPreviewSize({width: 100, height: 100});
+    }else{
+      CameraPreview.setPreviewSize({width: window.screen.width, height: window.screen.height});
+    }
+  },
+
+  showSupportedPictureSizes: function(){
+    CameraPreview.getSupportedPictureSizes(function(dimensions){
+      dimensions.forEach(function(dimension) {
+        console.log(dimension.width + 'x' + dimension.height);
+      });
+    });
+  },
+
+  init: function(){
+    document.getElementById('startCameraAboveButton').addEventListener('click', this.startCameraAbove, false);
+    document.getElementById('startCameraBelowButton').addEventListener('click', this.startCameraBelow, false);
+
+    document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
+    document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
+    document.getElementById('showButton').addEventListener('click', this.show, false);
+    document.getElementById('hideButton').addEventListener('click', this.hide, false);
+    document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+    document.getElementById('selectColorEffect').addEventListener('change', this.changeColorEffect, false);
+    document.getElementById('selectFlashMode').addEventListener('change', this.changeFlashMode, false);
+
+    if(navigator.userAgent.match(/Android/i)  == "Android"){
+      document.getElementById('zoomSlider').addEventListener('change', this.changeZoom, false);
+    }else{
+      document.getElementById('androidOnly').style.display = 'none';
+    }
+
+    window.smallPreview = false;
+    document.getElementById('changePreviewSize').addEventListener('click', this.changePreviewSize, false);
+
+    document.getElementById('showSupportedPictureSizes').addEventListener('click', this.showSupportedPictureSizes, false);
+
+    // legacy - not sure if this was supposed to fix anything
+    //window.addEventListener('orientationchange', this.onStopCamera, false);
   }
 };
-document.addEventListener('deviceready',app.init);
+
+document.addEventListener('deviceready', function(){
+  app.init();
+}, false);
 
 // var video, amountOfCameras;
 // var currentFacingMode = 'environment';
